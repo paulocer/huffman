2746ecbe8808b3609b33cb1d6378867913272366
paulocer <paulocer@gmail.com>
2019-05-20 21:07:02 -0300

    Add files via upload
    Primeiro commit

---
+++ read_and_write_files/src/main.asm
@@ -0,0 +1,62 @@
+
+%include "lib/asm_io.inc"
+
+
+segment .data
+  
+
+  filename db "test.txt", 0
+  buflen dw 2048
+  buffer db "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus eget est vitae semper. Vestibulum sed lectus eget metus ullamcorper bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras venenatis tortor arcu, ut auctor mi feugiat non. Nulla ullamcorper lacinia rutrum. Sed id tellus posuere, semper tellus ut, pretium magna. Etiam tincidunt luctus tellus, id tristique erat euismod et. Donec quis sagittis enim, tincidunt interdum ligula. Fusce fermentum, enim sed ultrices sollicitudin, leo tellus tristique est, vel accumsan augue nisi non dolor. Nullam interdum imperdiet purus eu volutpat.",0
+
+segment .bss
+
+;   buffer resb 2048 
+
+
+segment .text  
+
+%include "src/read_write_file.asm"
+
+        global  _asm_main
+_asm_main:
+    
+    ;push filename
+    ;push buffer
+    ;push buflen
+    ;call read_file
+    ;add esp, 12
+    
+    mov esi, buffer
+    cld 
+    print:
+    lodsb
+        cmp al, 0
+        je exit
+		;movzx eax, al
+		;push eax
+		;push vet_char
+		;push vet_count
+		;call contabiliza
+		;add esp, 12
+        call print_char
+    jmp print
+    exit:
+    call print_nl 
+   
+    leave                     
+    ret
+
+contabiliza:
+	push ebp
+	mov ebp, esp
+	
+	mov ebx, [ebp + 16]
+	mov edi, [ebp + 12]
+	mov esi, [ebp + 8]
+	
+
+
+
+
+
